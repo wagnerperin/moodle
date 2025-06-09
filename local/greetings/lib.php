39087498b000065c3caf99e5655d284c3f93aca8
Wagner de Andrade Perin <wagnerperin@gmail.com>
2025-06-09 16:43:24 -0300

    Add plugin link to navigation

--- local/greetings/lib.php
+++ local/greetings/lib.php
@@ -47,3 +47,34 @@ function local_greetings_get_greeting($user) {
 
     return get_string($langstr, 'local_greetings', fullname($user));
 }
+
+/**
+ * Insert a link to index.php on the site front page navigation menu.
+ *
+ * @param navigation_node $frontpage Node representing the front page in the navigation tree.
+ */
+function local_greetings_extend_navigation_frontpage(navigation_node $frontpage) {
+    $frontpage->add(
+        get_string('pluginname', 'local_greetings'),
+        new moodle_url('/local/greetings/index.php'),
+        navigation_node::TYPE_CUSTOM,
+    );
+}
+
+/**
+ * Add link to index.php into navigation block.
+ *
+ * @param global_navigation $root Node representing the global navigation tree.
+ */
+function local_greetings_extend_navigation(global_navigation $root) {
+    $node = navigation_node::create(
+        get_string('pluginname', 'local_greetings'),
+        new moodle_url('/local/greetings/index.php'),
+        navigation_node::TYPE_CUSTOM,
+        null,
+        null,
+        new pix_icon('t/message', '')
+    );
+
+    $root->add_node($node);
+}
